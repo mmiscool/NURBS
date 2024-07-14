f3f37990113714174aaa03b4bd047937d5ee09aa
Mike Molinari <mike@smbisoft.com>
2024-07-14 01:59:42 -0400

    Add tests for intersecting NURBS curves and surfaces
    Add tests for intersecting NURBS curves and surfaces
    
    * Add a test for intersecting two NURBS curves in the `Curve` class
    * Add a test for intersecting two NURBS surfaces in the `Surface` class
    
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/mmiscool/NURBS?shareId=150fbd05-147f-4b2c-ae8e-089916b47fb5).

--- tests/test-suite.js
+++ tests/test-suite.js
@@ -80,6 +80,21 @@ describe('Curve', () => {
     const derivative = curve.derivative(0.5, 1);
     expect(derivative).toEqual({ x: 1, y: 0, z: 0 });
   });
+
+  test('should intersect two NURBS curves', () => {
+    const controlPoints1 = [new Point(0, 0, 0), new Point(1, 1, 0), new Point(2, 0, 0)];
+    const degree1 = 2;
+    const knotVector1 = [0, 0, 0, 1, 1, 1];
+    const curve1 = new Curve(controlPoints1, degree1, knotVector1);
+
+    const controlPoints2 = [new Point(0, 1, 0), new Point(1, 0, 0), new Point(2, 1, 0)];
+    const degree2 = 2;
+    const knotVector2 = [0, 0, 0, 1, 1, 1];
+    const curve2 = new Curve(controlPoints2, degree2, knotVector2);
+
+    const intersections = curve1.intersect(curve2);
+    expect(intersections.length).toBeGreaterThan(0);
+  });
 });
 
 describe('Surface', () => {
@@ -113,6 +128,35 @@ describe('Surface', () => {
     const surface = new Surface(controlPoints, degrees, knotVectors);
     const derivative = surface.derivative(0.5, 0.5, 1, 1);
     expect(derivative).toEqual({ x: 1, y: 1, z: 0 });
+  });
+
+  test('should intersect two NURBS surfaces', () => {
+    const controlPoints1 = [
+      [new Point(0, 0, 0), new Point(1, 0, 0), new Point(2, 0, 0)],
+      [new Point(0, 1, 0), new Point(1, 1, 0), new Point(2, 1, 0)],
+      [new Point(0, 2, 0), new Point(1, 2, 0), new Point(2, 2, 0)]
+    ];
+    const degrees1 = [2, 2];
+    const knotVectors1 = [
+      [0, 0, 0, 1, 1, 1],
+      [0, 0, 0, 1, 1, 1]
+    ];
+    const surface1 = new Surface(controlPoints1, degrees1, knotVectors1);
+
+    const controlPoints2 = [
+      [new Point(0, 0, 1), new Point(1, 0, 1), new Point(2, 0, 1)],
+      [new Point(0, 1, 1), new Point(1, 1, 1), new Point(2, 1, 1)],
+      [new Point(0, 2, 1), new Point(1, 2, 1), new Point(2, 2, 1)]
+    ];
+    const degrees2 = [2, 2];
+    const knotVectors2 = [
+      [0, 0, 0, 1, 1, 1],
+      [0, 0, 0, 1, 1, 1]
+    ];
+    const surface2 = new Surface(controlPoints2, degrees2, knotVectors2);
+
+    const intersections = surface1.intersect(surface2);
+    expect(intersections.length).toBeGreaterThan(0);
   });
 });
 
